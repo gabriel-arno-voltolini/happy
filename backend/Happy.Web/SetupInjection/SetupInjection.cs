f0962f79ce497ad16b3b939493cfb0aea20e3c3e
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-15 18:53:38 -0300

    [Add] Organização do back-end concluida;

---
+++ backend/Happy.Web/SetupInjection/SetupInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Happy.Web.SetupInjection
+{
+    public static class SetupInjection
+    {
+        public static void SetupServicesDependencies(this IServiceCollection services)
+        {
+        }
+
+        public static void SetupRepositoriesDependencies(this IServiceCollection services)
+        {
+        }
+    }
+}
