de1d4c779f82fc8e046e99e7dea0f02636e0ce79
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-15 20:44:15 -0300

    [Add] BaseEntity;

---
+++ backend/Happy.Domain/Entities/BaseEntity/BaseEntity.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+
+namespace Happy.Domain.Entities.BaseEntity
+{
+    public abstract class BaseEntity
+    {
+        protected readonly IValidator _validator;
+
+        public int Id { get; protected set; }
+        public bool Deleted { get; protected set; }
+        public DateTime EntryTime { get; protected set; }
+        public DateTime? LastUpdate { get; protected set; }
+        public DateTime? Exclusion { get; protected set; }
+
+        protected BaseEntity(IValidator validator)
+        {
+            EntryTime = DateTime.Now;
+            _validator = validator;
+        }
+
+        public void Delete()
+        {
+            Deleted = true;
+            Exclusion = DateTime.Now;
+        }
+
+        public void Update()
+        {
+            LastUpdate = DateTime.Now;
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public ValidationResult Validade()
+        {
+            return _validator.Validate(this);
+        }
+    }
+}
