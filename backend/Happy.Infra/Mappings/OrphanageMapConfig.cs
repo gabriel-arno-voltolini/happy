04f5109175bd029064fe2c1960498e71772aaf95
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-16 08:25:16 -0300

    [Add] OrphanageMapConfig;

---
+++ backend/Happy.Infra/Mappings/OrphanageMapConfig.cs
@@ -0,0 +1,38 @@
+﻿using Happy.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Happy.Infra.Mappings
+{
+    internal class OrphanageMapConfig : IEntityTypeConfiguration<Orphanage>
+    {
+        public void Configure(EntityTypeBuilder<Orphanage> builder)
+        {
+            builder.ToTable("Orphanages");
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(128)
+                .IsUnicode(true)
+                .HasColumnName("Name");
+
+            builder.Property(c => c.About)
+                .HasMaxLength(2048)
+                .IsUnicode(true)
+                .HasColumnName("About");
+
+            builder.Property(c => c.Instructions)
+                .HasMaxLength(2048)
+                .IsUnicode(true)
+                .HasColumnName("Instructions");
+
+            builder.Property(c => c.Latitude)
+                .IsRequired()
+                .HasColumnName("Latitude");
+
+            builder.Property(c => c.Longitude)
+              .IsRequired()
+              .HasColumnName("Logitude");
+        }
+    }
+}
