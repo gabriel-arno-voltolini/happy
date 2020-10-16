9b1c505671b0ebe3fda561cae5d57a4b66d3e4fa
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-16 11:18:40 -0300

    [Add] Adição de validação customizada de coordenadas  para fluent validation;

---
+++ backend/Happy.Domain/Extensions/FluentValidation/CustomedValidations.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Happy.Domain.Extensions.Validating;
+
+namespace Happy.Domain.Extensions.FluentValidation
+{
+    public static class CustomedValidations
+    {
+        public static IRuleBuilderOptions<T, double> ValidateLatitude<T>(this IRuleBuilder<T, double> ruleBuilder) =>
+            ruleBuilder.Must(c => c.ValidateLatitude());
+
+        public static IRuleBuilderOptions<T, double> ValidateLongitude<T>(this IRuleBuilder<T, double> ruleBuilder) =>
+          ruleBuilder.Must(c => c.ValidateLongitude());
+    }
+}
