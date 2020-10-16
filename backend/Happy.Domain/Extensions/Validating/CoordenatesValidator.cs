2f816116fe9a43a68fdc15b75439bdc9fb90efde
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-16 11:17:36 -0300

    [Add] Extensão para validação de coordenadas;

---
+++ backend/Happy.Domain/Extensions/Validating/CoordenatesValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Happy.Domain.Extensions.Validating
+{
+    public static class CoordenatesValidator
+    {
+        public static bool ValidateLatitude(this double latitude)
+        {
+            return Math.Abs(latitude) <= 90;
+        }
+
+        public static bool ValidateLongitude(this double longitude)
+        {
+            return Math.Abs(longitude) <= 180;
+        }
+    }
+}
