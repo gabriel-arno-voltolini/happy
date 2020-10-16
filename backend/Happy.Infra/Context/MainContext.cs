56d43bfcf8681731fd6493a0fd2d02b040eab514
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-16 08:26:02 -0300

    [Add] MainContext

---
+++ backend/Happy.Infra/Context/MainContext.cs
@@ -0,0 +1,36 @@
+﻿using Happy.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Happy.Infra.Context
+{
+    public class MainContext : IdentityDbContext
+    {
+        public MainContext(DbContextOptions<MainContext> options)
+          : base(options)
+        {
+        }
+
+        public MainContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(MainContext).Assembly);
+        }
+
+        public DbSet<Orphanage> Orphanages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer("DefaultConnection");
+            }
+        }
+    }
+}
