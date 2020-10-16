d7b150c0a88b1cc400094de8532e686336cabaed
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-16 12:01:04 -0300

    [Add] OrphanageRepository;

---
+++ backend/Happy.Domain/Interfaces/Repositories/IOrphanageRepository.cs
@@ -0,0 +1,8 @@
+﻿using Happy.Domain.Entities;
+
+namespace Happy.Domain.Interfaces.Repositories
+{
+    public interface IOrphanageRepository : IGenericRepository<Orphanage>
+    {
+    }
+}
---
+++ backend/Happy.Infra/Repositories/EntityRepositories/OrphanageRepository.cs
@@ -0,0 +1,28 @@
+﻿using Happy.Domain.Entities;
+using Happy.Domain.Interfaces.Repositories;
+using Happy.Infra.Context;
+using Happy.Infra.Repositories.GenericRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Happy.Infra.Repositories.EntityRepositories
+{
+    public class OrphanageRepository : GenericRepository<Orphanage>, IOrphanageRepository
+    {
+        public OrphanageRepository(MainContext mainContext) : base(mainContext)
+        {
+        }
+
+        public override Task<bool> EntityExists(int id)
+        {
+            return _dbContext.Orphanages
+               .AnyAsync(c => c.Id == id && !c.Deleted);
+        }
+
+        public override Task<bool> EntityExists(Orphanage entity)
+        {
+            return _dbContext.Orphanages
+                .AllAsync(c => c.Id == entity.Id || c.Name == entity.Name);
+        }
+    }
+}
