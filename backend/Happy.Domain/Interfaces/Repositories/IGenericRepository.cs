34a262b8b81a09e9c441de2b327920148c9451f9
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-16 11:27:47 -0300

    [Add] IGenericRepository;

---
+++ backend/Happy.Domain/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using Happy.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Happy.Domain.Interfaces.Repositories
+{
+    interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> GetById(int id);
+
+        Task<int> Create(TEntity entity);
+
+        Task Create(IEnumerable<TEntity> entities);
+
+        Task Update(TEntity entity);
+
+        Task Delete(int id);
+
+        Task<List<TEntity>> GetAll();
+
+        Task<bool> EntityExists(TEntity entity);
+
+        Task<bool> EntityExists(int id);
+    }
+}
