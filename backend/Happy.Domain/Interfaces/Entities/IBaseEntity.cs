905071a20bb0af3ec54e3e89a1c7bce2095765c2
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-16 08:24:28 -0300

    -[Add] IOrphanage

---
+++ backend/Happy.Domain/Interfaces/Entities/IBaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+
+namespace Happy.Domain.Entities
+{
+    internal interface IBaseEntity
+    {
+        void Delete();
+
+        void Update();
+
+        void SetId(int id);
+
+        ValidationResult Validade();
+    }
+}
---
+++ backend/Happy.Domain/Interfaces/Entities/IOrphanage.cs
@@ -0,0 +1,9 @@
+﻿namespace Happy.Domain.Interfaces.Entities
+{
+    internal interface IOrphanage
+    {
+        void Update(string name, decimal latitude, decimal longitude);
+
+        void Update(string about, string instructions, bool opensOnWeekends);
+    }
+}
