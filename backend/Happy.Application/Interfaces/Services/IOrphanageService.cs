8cfdc6d5c8b7c9894259a16a80230d7273d6e593
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-17 09:50:17 -0300

    [Added] Orphanage Interface;

---
+++ backend/Happy.Application/Interfaces/Services/IOrphanageService.cs
@@ -0,0 +1,11 @@
+﻿using Happy.Application.AppFlowControl;
+using Happy.Application.Models.Orphanage;
+using System.Threading.Tasks;
+
+namespace Happy.Application.Interfaces.Services
+{
+    public interface IOrphanageService : IGenericService<OrphanageRequestModel, OrphanageResponseModel>
+    {
+        Task<DataResponse<OrphanageResponseModel>> GetAll(int id);
+    }
+}
