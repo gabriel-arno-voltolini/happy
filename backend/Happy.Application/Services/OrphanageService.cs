be4682e5c1389dde74d0cf6f3131d1b448466110
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-17 09:51:21 -0300

    [Added] OrphanageService;

---
+++ backend/Happy.Application/Services/OrphanageService.cs
@@ -0,0 +1,44 @@
+﻿using Happy.Application.AppFlowControl;
+using Happy.Application.Interfaces.Services;
+using Happy.Application.Models.Orphanage;
+using Happy.Domain.Interfaces.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace Happy.Application.Services
+{
+    public class OrphanageService : IOrphanageService
+    {
+        private readonly IOrphanageRepository _orphanageRepository;
+
+        public OrphanageService(IOrphanageRepository orphanageRepository)
+        {
+            _orphanageRepository = orphanageRepository;
+        }
+
+        public Task<DataResponse<OrphanageResponseModel>> Create(OrphanageRequestModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<OrphanageResponseModel>> GetAll(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataResponse<OrphanageResponseModel>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> Update(int id, OrphanageRequestModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
