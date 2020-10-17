e6adb3a09795f97c30c3eec1f0d271b4b489bc33
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-17 09:49:30 -0300

    [Added] AppFlowControl objects;

---
+++ backend/Happy.Application/AppFlowControl/DataResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Happy.Application.AppFlowControl
+{
+    public sealed class DataResponse<TEntity> : Response
+    {
+        public List<TEntity> Data { get; set; } = new List<TEntity>();
+
+        public DataResponse()
+        {
+        }
+
+        public DataResponse(List<TEntity> data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ backend/Happy.Application/AppFlowControl/Response.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Happy.Application.AppFlowControl
+{
+    public class Response
+    {
+        public bool Success => !Errors.Any();
+        public List<string> Errors { get; set; }
+
+        public Response()
+        {
+            Errors = new List<string>();
+        }
+
+        public void SetErrorsList(IList<ValidationFailure> errors)
+        {
+            errors.ToList().ForEach(err => Errors.Add(err.ToString()));
+        }
+
+        public string GetErrorMessage()
+        {
+            var builder = new StringBuilder();
+
+            foreach (string item in this.Errors)
+            {
+                builder.Append(item);
+            }
+            return builder.ToString();
+        }
+    }
+}
