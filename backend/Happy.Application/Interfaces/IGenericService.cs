ee9ead04bbe33bb674a5b4cabec7d66fc6aa23ad
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2020-10-17 09:50:42 -0300

    [Added] GenericService Interface;

---
+++ backend/Happy.Application/Interfaces/IGenericService.cs
@@ -0,0 +1,16 @@
+﻿using Happy.Application.AppFlowControl;
+using System.Threading.Tasks;
+
+namespace Happy.Application.Interfaces
+{
+    public interface IGenericService<TEntityRequest, TEntityResponse>
+    {
+        Task<DataResponse<TEntityResponse>> Create(TEntityRequest entity);
+
+        Task<DataResponse<TEntityResponse>> GetById(int id);
+
+        Task<Response> Update(int id, TEntityRequest entity);
+
+        Task<Response> Delete(int id);
+    }
+}
